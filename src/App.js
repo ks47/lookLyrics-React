b0f555c6c45e3963407f31fd428b2563c08494a7
ks47 <32562908+ks47@users.noreply.github.com>
2020-04-02 00:22:17 +0530

    Uploaded Project

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Navbar from "./components/layout/Navbar";
+import Index from "./components/layout/Index";
+
+import "./App.css";
+
+import { ContextController } from "./context";
+
+const App = () => {
+  return (
+    <ContextController>
+      <Router>
+        
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Index} />
+            </Switch>
+          </div>
+        
+      </Router>
+    </ContextController>
+  );
+};
+
+export default App;
